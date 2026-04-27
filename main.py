3c9a6c19a6722769e8679452d872c0467eaae786
Prasoon Kandel <prasoonkandel1029@gmail.com>
2026-04-27 22:16:50 +0545

    chore: created simple cli prototype to test essayGenerator utility

--- main.py
+++ main.py
@@ -2,14 +2,25 @@ import requests
 import json 
 import os
 from dotenv import load_dotenv
+from utils.essayGenerator import EssayGenerator
 
 load_dotenv()
 
-AI_URL = "https://ai.hackclub.com/proxy/v1/cphat/completions"
+AI_URL = "https://ai.hackclub.com/proxy/v1/chat/completions"
 
 AI_KEY = os.getenv("AI_KEY")
 
 MODEL = "google/gemini-3-flash-preview"
+
+
+EG = EssayGenerator(AI_KEY, AI_URL, MODEL)
+
+query = input("Enter the topic for the essay: ")
+
+essay = EG.generateEssay(query)
+
+print(essay)
+
 
 
     
