813dc241bdcddf687c6a6ce7c7f3943123e86d48
Prasoon Kandel <prasoonkandel1029@gmail.com>
2026-05-02 12:51:42 +0545

    chore: setting up flask server

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import os
 from dotenv import load_dotenv
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import requests
+import json
+import traceback
 from utils.essayGenerator import EssayGenerator
 import flet as ft
 
@@ -8,3 +13,25 @@ load_dotenv()
 AI_URL = os.getenv("AI_URL")
 AI_KEY = os.getenv("AI_KEY")
 AI_MODEL = os.getenv("AI_MODEL")
+
+app = Flask(__name__)
+
+CORS(app, resources={
+    r"/*": {
+        "origins": "*",
+        "methods": ["GET", "POST", "OPTIONS"],
+        "allow_headers": ["Content-Type", "Authorization"],
+        "supports_credentials": False
+    }
+})
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
+    return response
+
+@app.route('/', methods=['GET'])
+def home():
+    return "Essay Generator API"
