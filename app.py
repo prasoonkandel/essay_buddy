488d7e251fb69b557b41bc1547224a9f5e47724f
Prasoon Kandel <prasoonkandel1029@gmail.com>
2026-05-22 07:44:29 +0545

    fix: edited app.run() for production

--- app.py
+++ app.py
@@ -70,4 +70,7 @@ def gennerate_essay():
     return jsonify({"essay": essay}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(
+        host='0.0.0.0',
+        port=int(os.environ.get('PORT', 5000))
+    )
