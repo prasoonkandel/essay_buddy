8f9bb75711be942336946a1df608977dd7e1af14
Prasoon Kandel <prasoonkandel1029@gmail.com>
2026-05-23 14:47:40 +0545

    feat: add a simple cursor trail

---
+++ frontend/script.js
@@ -0,0 +1,39 @@
+function cursorAnimation() {
+  const cords = { x: 0, y: 0 };
+  const circles = document.querySelectorAll(".circle");
+
+  circles.forEach((circle) => {
+    circle.x = 0;
+    circle.y = 0;
+  });
+
+  window.addEventListener("mousemove", (event) => {
+    cords.x = event.clientX;
+    cords.y = event.clientY;
+  });
+
+  function animateCircles() {
+    let x = cords.x;
+    let y = cords.y;
+
+    circles.forEach((circle, index) => {
+      circle.style.left = x - 12 + "px";
+      circle.style.top = y - 12 + "px";
+      circle.x = x;
+      circle.y = y;
+
+      x += 3;
+      y += 3;
+
+      const nextCircle = circles[index + 1] || circles[0];
+      x += (nextCircle.x - x) * 0.35;
+      y += (nextCircle.y - y) * 0.35;
+    });
+
+    requestAnimationFrame(animateCircles);
+  }
+
+  animateCircles();
+}
+
+cursorAnimation();
